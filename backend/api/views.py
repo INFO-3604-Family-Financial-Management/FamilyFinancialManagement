f8dbef48072ec4125b3a8c52260d4db968c69af7
v-runnn <varun.maharaj5@my.uwi.edu>
2025-02-28 17:26:44 +0000

    I GETTING THINGS TO BE WRITTEN TO THE DATABASE Email needs to be also written to the db, but the fact of the matter is that the info entered in the frontend shows up in the db.sqlite3 file. The only thing that is not showing up is the email. I need to figure out why the email is not showing up in the db.sqlite3 file.

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,13 @@
+import logging
+
 from django.shortcuts import render
 from django.contrib.auth.models import User 
 from rest_framework import generics
 from .serializers import UserSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+
+# Logger instance for this module
+logger = logging.getLogger(__name__)
 
 # Create your views here.
 class CreateUserView(generics.CreateAPIView):
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -32,6 +32,27 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+    'loggers': {
+        'api': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    },
+}
+
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
