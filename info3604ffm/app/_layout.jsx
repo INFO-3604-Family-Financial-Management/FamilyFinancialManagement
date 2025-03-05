68a0cdc789cdbdf73d8a5eeb87b1046ea0cb2cdd
amirpersad <126283878+amirpersad@users.noreply.github.com>
2025-03-05 17:40:28 -0400

    fixed login redirect

--- info3604ffm/app/_layout.jsx
+++ info3604ffm/app/_layout.jsx
@@ -6,13 +6,11 @@ import AuthCheck from "@/components/AuthCheck";
 export default function RootLayout() {
   return(
     <AuthProvider>
-      <AuthCheck>
         <Stack>
           <Stack.Screen name="index" options={{headerShown: false}} />
           <Stack.Screen name="(auth)" options={{headerShown: false}} />
           <Stack.Screen name="(tabs)" options={{headerShown: false}} />
         </Stack>
-      </AuthCheck>
     </AuthProvider>
   )
 }
