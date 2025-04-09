ce4b7050ba3f48a994bd2f74f0217d9141c4cf25
Shivan001 <shivan.sookram@gmail.com>
2025-04-09 14:40:36 -0400

    changed the config yet again

--- info3604ffm/constants/config.js
+++ info3604ffm/constants/config.js
@@ -1,10 +1,10 @@
 // export const BACKEND_URL = 'http://192.168.236.92:8000'; //Amir IP
 // export const BACKEND_URL = 'http://192.168.163.92:8000';// Shivan IP
-export const BACKEND_URL = 'http://192.168.100.134:8000'; //Varun IP
+//export const BACKEND_URL = 'http://192.168.100.134:8000'; //Varun IP
 // export const BACKEND_URL = 'http://10.133.135.5:8000'; // Varun IP
 // export const BACKEND_URL = 'http://127.0.0.1:8000'; // Localhost IP
 
 
 
 
-//export const API_URL = 'https://familyfinancialmanagement.onrender.com'; // Render IP
+export const API_URL = 'https://familyfinancialmanagement.onrender.com'; // Render IP
